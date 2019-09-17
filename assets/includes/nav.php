5cd0237f47e8f9aa057942790a8255c77c86d065
Nilo-dev <43271698+Nilo-dev@users.noreply.github.com>
2019-09-16 22:23:54 -0300

    Add files via upload

---
+++ assets/includes/nav.php
@@ -0,0 +1,34 @@
+<header>
+  <div class="container">
+    <h1 class="text-center">Kodika</h1>
+    <h6 class="text-center">by Aspas Games</h6>
+  </div>
+  <nav>
+    <ul class="nav nav-tabs justify-content-center nav-justified">
+      <li class="nav-item">
+        <a class="nav-link active" href="?page=home">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Biblioteca</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Jogos</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Perfil</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Visualizar Perfil</a>
+          <a class="dropdown-item" href="#">Editar seus dados</a>
+          <a class="dropdown-item" href="#">Logout</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Administrador</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="?page=gamereg">Cadastrar Jogo</a>
+          <a class="dropdown-item" href="#">Cadastrar Gênero</a>
+        </div>
+      </li>
+    </ul>
+  </nav>
+</header>
